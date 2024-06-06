aa2a539d5538011a2ff2f5f50ae671f74d646ec4
chickiexd <contact@chickiexd.com>
2024-06-06 17:08:49 +0200

    removed notify

--- lua/chickie/plugins/noice.lua
+++ lua/chickie/plugins/noice.lua
@@ -3,6 +3,9 @@ return
   "folke/noice.nvim",
   event = "VeryLazy",
   opts = {
+      notify = {
+        enable = false,
+      }
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -10,6 +13,14 @@ return
     -- OPTIONAL:
     --   `nvim-notify` is only needed, if you want to use the notification view.
     --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-  }
+    -- "rcarriga/nvim-notify",
+  },
+  config = function()
+    require("noice").setup({
+      notify = {
+        enable = false,
+
+      }
+    })
+  end,
 }
