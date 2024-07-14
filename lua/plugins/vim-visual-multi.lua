588f2af9780f6762b9e74a511785bf1b6a4e190d
chickiexd <contact@chickiexd.com>
2024-07-14 15:38:10 +0200

    added vim-visual-multi plugin

---
+++ lua/plugins/vim-visual-multi.lua
@@ -0,0 +1,15 @@
+return
+    {
+        "mg979/vim-visual-multi",
+        branch = "master",
+        dependencies = {  },
+        config = function()
+            -- local visual_multi = require("visual-multi")
+            -- visual_multi.setup({})
+
+            -- Toggle previous & next buffers stored within Harpoon list
+            -- vim.keymap.set("n", "<C-f>", function() harpoon:list():prev() end)
+            -- vim.keymap.set("n", "<C-g>", function() harpoon:list():next() end)
+        end
+
+    }
