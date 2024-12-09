b415c75ca2c20432fce7ba6e5e5f64c8f9017ee7
chickiexd <contact@chickiexd.com>
2024-12-09 04:14:01 +0100

    added nvim leetcode

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,22 @@
+return {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        "nvim-lua/plenary.nvim", -- required by telescope
+        "MunifTanjim/nui.nvim",
+
+        -- optional
+        "nvim-treesitter/nvim-treesitter",
+        "rcarriga/nvim-notify",
+        "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+        lang = "python3",
+        storage = {
+            home = "/home/chickie/personal/leetcode",
+            cache = "/home/chickie/personal/leetcode/.cache"
+        }
+        -- configuration goes here
+    },
+}
