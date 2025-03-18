f823f140707b0925ecc50ee02b664fa61ae9acd9
chickiexd <contact@chickiexd.com>
2025-03-18 18:15:35 +0100

    changed look of indent_line

--- lua/plugins/indent_line.lua
+++ lua/plugins/indent_line.lua
@@ -1,8 +1,13 @@
 return
-{   -- Add indentation guides even on blank lines
+{ -- Add indentation guides even on blank lines
   'lukas-reineke/indent-blankline.nvim',
   -- Enable `lukas-reineke/indent-blankline.nvim`
   -- See `:help ibl`
   main = 'ibl',
   opts = {},
+  config = function()
+    require('ibl').setup({
+      scope = { enabled = true, show_start = false, show_end = false },
+    })
+  end,
 }
